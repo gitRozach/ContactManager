06a81e7c00f828e71bfc62ac0403762b2329257b
gitRozach <roman.kindler.1997@googlemail.com>
2024-06-17 17:24:07 +0200

    Fixed HeaderBar Delete & Edit Button functionality

---
+++ ContactsManager/src/main/api/src/main/java/dev/rozach/contactsmanager/Config.java
@@ -0,0 +1,10 @@
+package dev.rozach.contactsmanager;
+
+public class Settings {
+    public static final int MAX_NAME_LENGTH = 48;
+    public static final int MAX_TEAM_LENGTH = 48;
+    public static final int MAX_TITLE_LENGTH = 48;
+    public static final int MAX_EMAIL_LENGTH = 48;
+    public static final int MAX_PHONE_NUMBER_LENGTH = 48;
+    public static final int MAX_IMAGE_URL_LENGTH = 100;
+}
